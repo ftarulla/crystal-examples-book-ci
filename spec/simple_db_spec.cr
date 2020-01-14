7c2723fed2de975a3e776964e1996066ff70ee19
Franciscello <ftarulla@gmail.com>
2020-01-14 15:02:48 -0300

    Fix `query` command

--- spec/simple_db_spec.cr
+++ spec/simple_db_spec.cr
@@ -3,6 +3,6 @@ require "mysql"
 
 it "connects to the database" do
   DB.open ENV["DATABASE_URL"] do |db|
-    db.query("SELECT 'foo'", as: String).should eq "foo"
+    db.query_one("SELECT 'foo'", as: String).should eq "foo"
   end
 end
