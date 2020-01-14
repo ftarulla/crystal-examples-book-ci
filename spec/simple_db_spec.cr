eb816bf6a62b84e6b08e0dffa52f140976a923bc
Franciscello <ftarulla@gmail.com>
2020-01-14 14:58:32 -0300

    Using `open` method instead of `connect`

--- spec/simple_db_spec.cr
+++ spec/simple_db_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 require "mysql"
 
 it "connects to the database" do
-  DB.connect ENV["DATABASE_URL"] do |cnn|
-    cnn.query("SELECT 'foo'", as: String).should eq "foo"
+  DB.open ENV["DATABASE_URL"] do |db|
+    db.query("SELECT 'foo'", as: String).should eq "foo"
   end
 end
